d6ed447cab368b576129e3d7b26bb6573c9653d3
varun singh <chefvarunsingh@gmail.com>
2024-11-23 14:58:48 +0530

    Built web
    Branch: master
    Commit: 8c20d6033ed05e7202f9cfb60aba4d8ca035b9fb (dirty)
    
    package:peanut 5.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2148023791"
+    serviceWorkerVersion: "3675606421"
   }
 });
