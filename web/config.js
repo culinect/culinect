e193a44f7af195a436dcbdb7c36bca5164cd8820
varun singh <chefvarunsingh@gmail.com>
2024-11-23 21:12:46 +0530

    Update configuration and security settings

---
+++ web/config.js
@@ -0,0 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBXbbSsDVH1eVE297nCyU9euFilYmJWH1U",
+    authDomain: "culinect-social.firebaseapp.com",
+    projectId: "culinect-social",
+    storageBucket: "culinect-social.appspot.com",
+    messagingSenderId: "553932496521",
+    appId: "1:553932496521:web:bec35d05cb23cd9beef058"
+};
+
+var branchConfig = {
+
+    liveKey: "key_live_nwps1OYseoCnJGXIxKj7Sckhxti7wWJP"
+};
+
+var fcmConfig = {
+    vapidKey: "BOfFs1FcP9sjpHFx3ph6GEQe3x0sU1RS1NJWDV1HxBp8GHxSqWrdmYmqjVGHbE0lrQtCaOKVF5KqFOPC_qEcWQ4"
+};
