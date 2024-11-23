a957083e82554a64ea9ad36ba0770721291ce6cb
varun singh <chefvarunsingh@gmail.com>
2024-11-23 11:45:18 +0530

    Built web
    Branch: master
    Commit: 8c20d6033ed05e7202f9cfb60aba4d8ca035b9fb (dirty)
    
    package:peanut 5.0.0

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,23 @@
-importScripts("https://www.gstatic.com/firebasejs/9.x.x/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.x.x/firebase-messaging.js");
-
+importScripts('https://www.gstatic.com/firebasejs/9.9.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.9.0/firebase-messaging-compat.js');
 firebase.initializeApp({
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: 'AIzaSyBXbbSsDVH1eVE297nCyU9euFilYmJWH1U',
+  appId: '1:553932496521:web:bec35d05cb23cd9beef058',
+  messagingSenderId: '553932496521',
+  projectId: 'culinect-social',
+  authDomain: 'culinect-social.firebaseapp.com',
+  storageBucket: 'culinect-social.appspot.com',
+  measurementId: 'G-RQ20V98BR9'
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message:', payload);
-
+  console.log("onBackgroundMessage", payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icons/icon-192x192.png'
+    icon: '/icons/Icon-192.png'
   };
-
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });
